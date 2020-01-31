5fe37e46a2086df133d6c0ab6ba0842ffedfb8e6
FearBlackScarab <fearblackscarab@gmail.com>
2020-01-31 09:08:09 -0600

    added tax/exchange rate

--- app.js
+++ app.js
@@ -1,20 +1,45 @@
 let cart=[]
 ;
+let changecurrency =document.getElementById('currency');
+;
 let items = document.querySelectorAll('.item')
 ;
+// currency
+// exchange rate
+let currencyrate = 1
+;
+let currencysymbol = '&dollar;'
+;
+changecurrency.addEventListener('change',function(){
+    let country =this.value;
+    switch (country){
+        case "us":
+            currencyrate = 1;
+            currencysymbol = '&dollar;';
+            break;
+        case 'uk':
+            currency = 1.5;
+            currencysymbol = '&pound;';
+            break;
+            default:
+                break;
+    }
+    updateTotal();
+});
 for (let i=0; i < items.length;i++){
     const item = items[i];
+
     item.addEventListener('click',function(){
         let found = false ;
         let price = item.getAttribute('data-price');
         let name = item.innerText
+
 for (let j=0; j < cart.length;j++){
     const cartItem = cart[j]
     if(cartItem.name == item.innerText){
         found= true;
         cart[j].qty++;
-    }
-}
+    }}
 if(!found){
     cart.push({name, price, qty:1});
     }
@@ -23,7 +48,10 @@ if(!found){
     function updateItems(){
         let numberItems = document.getElementById('numberItems')
         numberItems.innerText = cart.length;
+    
     };
+});
+    // function updatecurrencyrate()
     function updateTotal(){
         let cartTotal =document.getElementById('cartTotal');
         let total=0;
@@ -31,7 +59,9 @@ if(!found){
             const item=cart[i]
             total += item.price * item.qty;
         }
-        cartTotal.innerText=total.toFixed(2);
+        // tax
+        total += total * 0.7;
+        total /= currencyrate;
+        cartTotal.innerHTML=`${currencysymbol}${total.toFixed(2)}`;
     }
-});
 }
